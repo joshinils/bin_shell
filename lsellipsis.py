578e689f7c89227ff7710535c13e605819b6f0e9
joshinils <joshua.laskowski@web.de>
2022-05-14 00:29:43 +0200

    do not ignore preceding characters which are not digit-chars

--- lsellipsis.py
+++ lsellipsis.py
@@ -12,45 +12,53 @@ def add_ellipses(ls: List[str]) -> List[str]:
     prev_was_match = False
     omitted_count = 0
     ls_return = []
+    prev_char_match = None
 
     for f in ls:
         this_index += 1
-        matches = re.findall("[0-9]+", f)
-        concat_matches = ''.join(matches)
+        digit_matches = re.findall(r"[0-9]+", f)
+        concat_matches = ''.join(digit_matches)
+
+        char_matches = re.findall(r"^.+?(?=[0-9])", f)
+        concat_char_match = ''.join(char_matches)
 
         if len(concat_matches) > 0:
             number_match = int(concat_matches)
         else:
             number_match = -1
 
-        if (        len(concat_matches) > 0 # there are numbers
-                and prev_number == number_match -1 # continuous
-            ):
+        if(len(concat_matches) > 0  # there are numbers
+            and prev_number == number_match - 1  # continuous
+            and prev_char_match == concat_char_match  # beginning string is same
+           ):
             # don't print f
             prev_was_match = True  # for next iteration
-            omitted_count += 1 # for next iteration
+            omitted_count += 1  # for next iteration
 
-            if this_index+1 == len(ls): # is last
+            if this_index + 1 == len(ls):  # is last
                 ls_return.append("...")
                 ls_return.append(f)
                 return ls_return
-            prev_f = f # for next iteration
+            prev_f = f  # for next iteration
         else:
             if prev_was_match:
-                prev_was_match = False # for next iteration
+                prev_was_match = False  # for next iteration
 
                 if omitted_count > 1:
                     ls_return.append("...")
                 ls_return.append(prev_f)
 
             ls_return.append(f)
-            omitted_count = 0 # reset
+            omitted_count = 0  # reset
 
-        prev_number = number_match # for next iteration
+        # for next iteration
+        prev_number = number_match
+        prev_char_match = concat_char_match
 
     return ls_return
 
+
 if __name__ == '__main__':
-    ls = add_ellipses( subprocess.run(["ls","-1A"],text=True,stdout=subprocess.PIPE).stdout.splitlines() )
+    ls = add_ellipses(subprocess.run(["ls", "-1A"], text=True, stdout=subprocess.PIPE).stdout.splitlines())
     for i in ls:
         print(i)
