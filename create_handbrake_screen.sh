4b4137520b06a3a404e0a9ba3ca51302154347f2
joshinils <joshua.laskowski@web.de>
2022-10-10 06:54:46 +0200

    add create_handbrake_screen.sh

---
+++ create_handbrake_screen.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# to be invoked by cron
+
+# check if a screen session with name "handbrake" exists,
+# if not, create it
+
+if ! screen -list | grep -q "handbrake"; then
+    screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data/filme_neu/untouched/do.sh"
+fi
