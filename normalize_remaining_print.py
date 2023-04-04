f83c3a5874548d4ccac6f269f74ea978b270e18f
joshinils <joshua.laskowski@web.de>
2023-04-04 11:36:48 +0200

    sort by duration * streams total

--- normalize_remaining_print.py
+++ normalize_remaining_print.py
@@ -2,6 +2,7 @@
 
 import os
 from typing import Tuple, Dict
+import subprocess
 
 extension_keep: Tuple[str] = (
     "ogg",
@@ -11,9 +12,6 @@ extension_keep: Tuple[str] = (
     "mp4",
     "mkv",
 )
-
-def concat_size_tuple(tup) -> str:
-    return 
 
 
 def make_size_str(size_B: int) -> str:
@@ -57,7 +55,7 @@ def make_size_str(size_B: int) -> str:
 def main():
     filenames = next(os.walk(os.getcwd()), (None, None, []))[2]  # [] if no file
 
-    print()
+    # print()
     to_remove = []
     for name in filenames:
         if name.endswith(".log"):
@@ -75,13 +73,36 @@ def main():
 
     size_name = []
     for name in to_keep:
-        size_name.append((os.path.getsize(name), name))
+        process_ffprobe = subprocess.Popen(["ffprobe", "-i", name, "-show_streams", "-select_streams", "a"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process_grep = subprocess.Popen(["grep", "channels"], stdin=process_ffprobe.stdout, stdout=subprocess.PIPE)
+        output, error = process_grep.communicate()
+        result = output.decode().strip()
+        process_ffprobe = subprocess.Popen("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1".split() + [name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = process_ffprobe.communicate()
+        duration = float(output)
+        print(duration)
 
-    size_name.sort(key=lambda x: x[0], reverse=True)
+
+        audio_channels: Dict[int, int] = {2:0, 6:0}
+        channels_count = 0
+        for line in result.split("channels="):
+            stripped_line = line.strip()
+            if len(stripped_line) > 0:
+                try:
+                    num_channels = int(stripped_line)
+                    audio_channels[num_channels] = audio_channels.get(num_channels, 0) + 1
+                    channels_count += num_channels
+                except:
+                    pass
+        print(audio_channels)
+
+        size_name.append((os.path.getsize(name), name, audio_channels, channels_count, duration))
+
+    size_name.sort(key=lambda x: x[3] * x[4], reverse=True)
 
     print("Waiting:")
-    for size, name in size_name:
-        print(f"{make_size_str(size)}, {name}")
+    for size, name, channels_dict, num_channels, duration in size_name:
+        print(f"{make_size_str(size)}, {str(channels_dict): >12}={num_channels: >2g} {name}")
 
 
 if __name__ == "__main__":
