0cc7ecd82e8bd443699d0fed62eac407411c4603
joshinils <joshua.laskowski@web.de>
2023-05-13 15:00:40 +0200

    speed up printing by writing metadata to file.pkl

--- normalize_remaining_print.py
+++ normalize_remaining_print.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import pickle
 from typing import Dict, Tuple
 
 from normalize import get_amount_of_audio_streams
@@ -54,6 +55,25 @@ def make_size_str(size_B: int) -> str:
         return f"{size_B / size:7.2f} {name}"
 
 
+pickled_filename = "file_meta.pkl"
+
+
+def pickle_load_filemeta() -> Dict[str, Tuple[int, int]]:
+    try:
+        with open(pickled_filename, 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return dict()
+    except Exception as e:
+        print(type(e), e)
+        return dict()
+
+
+def pickle_save_filemeta(metadata: Dict[str, Tuple[int, int]]) -> None:
+    with open(pickled_filename, 'wb') as f:
+        pickle.dump(metadata, f)
+
+
 def main():
     filenames = next(os.walk(os.getcwd()), (None, None, []))[2]  # [] if no file
 
@@ -62,6 +82,11 @@ def main():
     for name in filenames:
         if name.endswith(".log"):
             to_remove.append(name[:-4])
+            for i in range(999):
+                ending = f".audio-{i:03d}.mkv.log"  # Kill Bill: Volume 2_2004 - BD.mkv.audio-001.mkv.log
+                if name.endswith(ending):
+                    to_remove.append(name.replace(ending, ""))
+                    break
 
     to_keep = []
     for name in filenames:
@@ -73,9 +98,21 @@ def main():
             continue
         to_keep.append(name)
 
+    filemeta_data = pickle_load_filemeta()
+
+    keep_file_meta = {}
+    for filename, meta_tuple in filemeta_data.items():
+        if filename in to_keep:
+            keep_file_meta[filename] = meta_tuple
+
     streams_size_name = []
     for name in to_keep:
-        streams_size_name.append((get_amount_of_audio_streams(name), os.path.getsize(name), name))
+        if name not in keep_file_meta:
+            keep_file_meta[name] = (get_amount_of_audio_streams(name), os.path.getsize(name))
+        stream_count, filesize = keep_file_meta.get(name)
+        streams_size_name.append((stream_count, filesize, name))
+
+    pickle_save_filemeta(keep_file_meta)
 
     streams_size_name.sort(key=lambda x: x[1], reverse=False)
     streams_size_name.sort(key=lambda x: x[0], reverse=False)
