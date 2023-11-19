57cd33eeda41422f8d4b144d62a4816580c33c6e
joshinils <joshua.laskowski@web.de>
2023-11-19 07:51:32 +0100

    remove space necessary for terminal priunting

--- dashboard_daily.sh
+++ dashboard_daily.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 LC_ALL=de_DE.utf8 date +"%A, %d. %B %Y W%W T%j %Z %:::z"
-curl -s "wttr.in/Frohnau?format=%F0%9F%8C%84%D+A+%F0%9F%8C%85%S+A+%F0%9F%8F%9E%EF%B8%8F+%z+A+%F0%9F%8C%87%s+A+%F0%9F%8C%8C%d+A+%m%M" | sed 's/:[0-9]\{2\} A//g'; echo
+curl -s "wttr.in/Frohnau?format=%F0%9F%8C%84%D+A+%F0%9F%8C%85%S+A+%F0%9F%8F%9E%EF%B8%8F%z+A+%F0%9F%8C%87%s+A+%F0%9F%8C%8C%d+A+%m%M" | sed 's/:[0-9]\{2\} A//g'; echo
 curl -s "wttr.in/Frohnau?format=%c%t+%F0%9F%92%A7%p/3h+%F0%9F%92%A8%w+%F0%9F%8C%AB%EF%B8%8F+%h+%P+%F0%9F%94%86%u/12"; echo
