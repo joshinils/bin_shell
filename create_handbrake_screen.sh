574fc27410b1b95cbcdc0094c73ef275179d213b
joshinils <joshua.laskowski@web.de>
2023-12-13 22:11:15 +0100

    prefer remuxing on old nas, to clear that backlog

--- create_handbrake_screen.sh
+++ create_handbrake_screen.sh
@@ -6,9 +6,9 @@
 # if not, create it
 
 if ! screen -list | grep -q "handbrake"; then
-    if [ "$(find -- /media/wd4-r1/data_36/filme_neu/untouched/*.json 2>/dev/null | wc -l)" != 0 ] ; then
-        screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data_36/filme_neu/untouched/do.sh -i '${1}'"
-    else
+    if [ "$(find -- /media/wd4-r1/data/filme_neu/untouched/*.json 2>/dev/null | wc -l)" != 0 ] ; then
         screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data/filme_neu/untouched/do.sh -i '${1}'"
+    else
+        screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data_36/filme_neu/untouched/do.sh -i '${1}'"
     fi
 fi
