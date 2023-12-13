1f66c9a5dfd419f3b97faadf2f848faef5b2e482
jl <jl@l-nas36.pt>
2023-12-13 22:07:29 +0100

    prefer remuxing on l-nas36

--- create_handbrake_screen.sh
+++ create_handbrake_screen.sh
@@ -6,5 +6,9 @@
 # if not, create it
 
 if ! screen -list | grep -q "handbrake"; then
-    screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data/filme_neu/untouched/do.sh -i '${1}'"
+    if [ "$(find -- /media/wd4-r1/data_36/filme_neu/untouched/*.json 2>/dev/null | wc -l)" != 0 ] ; then
+        screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data_36/filme_neu/untouched/do.sh -i '${1}'"
+    else
+        screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data/filme_neu/untouched/do.sh -i '${1}'"
+    fi
 fi
