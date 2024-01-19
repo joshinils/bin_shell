d30f6a9f6bef739761743159f89b0ada3675f21c
joshinils <joshua.laskowski@web.de>
2024-01-20 00:04:49 +0100

    mediainfo -f

--- gen_mediainfo.sh
+++ gen_mediainfo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-find . -maxdepth 1 -type f -name "*.mkv" -exec sh -c 'mediainfo "$1" > "$1.mediainfo"' shell {} \;
+find . -maxdepth 1 -type f -name "*.mkv" -exec sh -c 'mediainfo -f "$1" > "$1.mediainfo"' shell {} \;
 code . -n  -- *.mediainfo
