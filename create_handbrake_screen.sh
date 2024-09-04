1294fe964d965053f3073c470a37a9b01dd65e91
joshinils <joshua.laskowski@web.de>
2024-09-04 19:51:06 +0200

    sleep random if no screen session exists [0.1, 0.9]s

--- create_handbrake_screen.sh
+++ create_handbrake_screen.sh
@@ -5,6 +5,14 @@
 # check if a screen session with name "handbrake" exists,
 # if not, create it
 
+echo $RANDOM > /dev/null
+if ! screen -list | grep -q "handbrake"; then
+    # sleep random amount between 0.1 and 0.9 seconds
+    sleep "$(echo "$(shuf -i 1000-9000 -n 1)/10000" | bc -l)"
+else
+    exit 1
+fi
+
 if ! screen -list | grep -q "handbrake"; then
     if [ "$(find -- /media/wd4-r1/data/filme_neu/untouched/*.json 2>/dev/null | wc -l)" != 0 ] ; then
         screen -dm -S handbrake /usr/bin/zsh -c "/media/wd4-r1/data/filme_neu/untouched/do.sh -i '${1}'"
